4ad0e58a4277114856e5e6cd42a978cbc23bebf5
Barbarella <barbarella6666666@protonmail.com>
2026-04-17 14:21:44 -0300

    fix: previous video was removed from server

--- tests/crawlers/test_cases/tranny_one.py
+++ tests/crawlers/test_cases/tranny_one.py
@@ -2,12 +2,12 @@ DOMAIN = "tranny.one"
 TEST_CASES = [
     # Video
     (
-        "https://www.tranny.one/view/1156655/",
+        "https://es.tranny.one/view/1195231",
         [
             {
-                "url": r"re:https://stream.tranny.one/.+/3137805.mp4",
-                "filename": "Femboy and the Hunk [1156655].mp4",
-                "referer": "https://www.tranny.one/view/1156655",
+                "url": r"re:https://stream.tranny.one/.+/3176393.mp4",
+                "filename": "Travesti pelirroja voluptuosa muy intensa y sudorosa. [1195231].mp4",
+                "referer": "https://es.tranny.one/view/1195231",
                 "datetime": None,
             }
         ],
